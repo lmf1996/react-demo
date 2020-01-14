ce5d3d76c87c0d23af4ae030bf3c1fc03cc8b839
limengfan <limenfan@cmiot.chinamobile.com>
2020-01-14 17:43:36 +0800

    css无法引入

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,24 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports={
-    entry:'./src/index.js',
-    output:{
-        path:path.resolve(process.cwd(),"dist"),
-        filename:'[name].[chunkHash:8].js'
-    }, 
-    plugins:[
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(process.cwd(), "dist"),
+        filename: '[name].[chunkHash:8].js'
+    },
+    plugins: [
         new HtmlWebpackPlugin({
-            title:'webpack',
-            template:'public/index.html'
+            title: 'webpack',
+            template: 'public/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'[name].[chunkHash:8].css'
         })
     ],
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                          esModule: true
+                      } 
+                    },
+                    'css-loader',
+                    'style-loader'                    
+                ],
+            },
         ],
     }
 }
